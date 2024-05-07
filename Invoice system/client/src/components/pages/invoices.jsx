e3c97324a7cf87f97bf646089ca868e83a88ef5d
Chavdar Stoilov <stoilov.chavdar@gmail.com>
2024-05-07 16:22:54 +0300

    Change Invoice colums in table page, add suffix on amount and remove lables

--- Invoice system/client/src/components/pages/invoices.jsx
+++ Invoice system/client/src/components/pages/invoices.jsx
@@ -1,24 +1,18 @@
-import { Badge } from '@mantine/core';
+import { Badge, Button, NumberFormatter  } from "@mantine/core";
 
-
-export default function Invoices({data}) {
+export default function Invoices({ data }) {
     return (
         <div className="products-area-wrapper tableView">
             <div className="products-header">
                 <div className="product-cell">ID</div>
-                <div className="product-cell category">Items</div>
                 <div className="product-cell status-cell">Status</div>
-                <div className="product-cell sales">Quantity</div>
-                <div className="product-cell stock">Price</div>
                 <div className="product-cell price">Amount</div>
+                <div className="product-cell price"></div>
             </div>
 
             <div className="products-row">
                 <div className="product-cell">
-                    <span>Ocean</span>
-                </div>
-                <div className="product-cell category">
-                    <span className="cell-label">Category:</span>Furniture
+                    <span>0000001</span>
                 </div>
                 <div className="product-cell status-cell">
                     <span className="cell-label">Status:</span>
@@ -26,14 +20,19 @@ export default function Invoices({data}) {
                         Active
                     </Badge>
                 </div>
-                <div className="product-cell sales">
-                    <span className="cell-label">Sales:</span>11
-                </div>
-                <div className="product-cell stock">
-                    <span className="cell-label">Stock:</span>36
-                </div>
+
                 <div className="product-cell price">
-                    <span className="cell-label">Price:</span>$560
+                    <span className="cell-label">Price:</span><NumberFormatter value={100000} suffix=" лв." thousandSeparator />
+                </div>
+
+                <div className="product-cell">
+                    <Button
+                        variant="gradient"
+                        size="compact-sm"
+                        gradient={{ from: "blue", to: "cyan", deg: 90 }}
+                    >
+                        Open Invoice
+                    </Button>
                 </div>
             </div>
         </div>
