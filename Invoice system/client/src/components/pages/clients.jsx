0338fbcc32f2c86ea359bc7bb4a508ed57648c9b
Chavdar Stoilov <80538498+ChavdarStoilov@users.noreply.github.com>
2024-05-13 09:32:47 +0300

    Added loader and to another pageS

--- Invoice system/client/src/components/pages/clients.jsx
+++ Invoice system/client/src/components/pages/clients.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
-import { Button } from "@mantine/core";
+import { Button, Loader } from "@mantine/core";
 import ClientsItemsTable from "../utilsComponents/clientsItemsTable";
 import * as api from "../../api/data";
 import NewClientsModal from "../modals/newClients";
-
 
 export default function Clients() {
     const [clientsData, setClientsData] = useState({});
     const [openNewClients, setOpenNewClients] = useState(false);
     const [refresh, setRefresh] = useState(false);
+    const [loader, setLoader] = useState(true);
 
     const openNewClientsHander = () => setOpenNewClients(true);
     const closeNewClientsHander = () => setOpenNewClients(false);
@@ -24,27 +24,49 @@ export default function Clients() {
             })
             .catch((error) => {
                 console.log(error);
+            })
+            .finally(() => {
+                setLoader(false);
             });
     }, [refresh]);
 
     return (
         <>
-            {openNewClients && <NewClientsModal open={openNewClients} close={closeNewClientsHander} refreshing={refreshHander}/>}
-            <div className="app-content-header">
-                <h1>Clients</h1>
-                <Button variant="filled" onClick={openNewClientsHander}>Add New Clients</Button>
-            </div>
-            <div className="products-area-wrapper tableView">
-                <div className="products-header">
-                    <div className="product-cell">ID</div>
-                    <div className="product-cell">First name</div>
-                    <div className="product-cell">Last Name</div>
-                    <div className="product-cell">Phone</div>
-                    <div className="product-cell"></div>
-                </div>
+            {!loader ? (
+                <>
+                    {openNewClients && (
+                        <NewClientsModal
+                            open={openNewClients}
+                            close={closeNewClientsHander}
+                            refreshing={refreshHander}
+                        />
+                    )}
+                    <div className="app-content-header">
+                        <h1>Clients</h1>
+                        <Button variant="filled" onClick={openNewClientsHander}>
+                            Add New Clients
+                        </Button>
+                    </div>
+                    <div className="products-area-wrapper tableView">
+                        <div className="products-header">
+                            <div className="product-cell">ID</div>
+                            <div className="product-cell">First name</div>
+                            <div className="product-cell">Last Name</div>
+                            <div className="product-cell">Phone</div>
+                            <div className="product-cell"></div>
+                        </div>
 
-                <ClientsItemsTable clients={clientsData}/>
-            </div>
+                        <ClientsItemsTable clients={clientsData} />
+                    </div>
+                </> 
+            ) : (
+                <Loader
+                    color="blue"
+                    type="dots"
+                    className="loader"
+                    size={50}
+                />
+            )}
         </>
     );
 }
--- Invoice system/client/src/components/pages/invoices.jsx
+++ Invoice system/client/src/components/pages/invoices.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
-import { Button } from "@mantine/core";
+import { Button, Loader } from "@mantine/core";
 import NewInvoiceModal from "../modals/newInvoice";
 import InvoiceItemsTable from "../utilsComponents/invoiceItemsTable";
 import * as api from "../../api/data";
-
 
 export default function Invoices() {
     const [invoiceData, setInvoiceData] = useState(false);
     const [openNewInvoice, setOpenNewInvoice] = useState(false);
     const [refresh, setRefresh] = useState(false);
+    const [loader, setLoader] = useState(true);
 
     const openNewInvoiceHander = () => setOpenNewInvoice(true);
     const closeNewInvoiceHander = () => setOpenNewInvoice(false);
@@ -24,27 +24,46 @@ export default function Invoices() {
             })
             .catch((error) => {
                 console.log(error);
+            })
+            .finally(() => {
+                setLoader(false);
             });
     }, [refresh]);
 
     return (
         <>
-            {openNewInvoice && <NewInvoiceModal open={openNewInvoice} close={closeNewInvoiceHander} refreshing={refreshHander}/>}
-            <div className="app-content-header">
-                <h1>Invoices</h1>
-                <Button variant="filled" onClick={openNewInvoiceHander}>Add New Invoice</Button>
-            </div>
-            <div className="products-area-wrapper tableView">
-                <div className="products-header">
-                    <div className="product-cell">ID</div>
-                    <div className="product-cell status-cell">Status</div>
-                    <div className="product-cell price">Cusotmer</div>
-                    <div className="product-cell price">Amount</div>
-                    <div className="product-cell price"></div>
-                </div>
+            {!loader ? (
+                <>
+                    {openNewInvoice && (
+                        <NewInvoiceModal
+                            open={openNewInvoice}
+                            close={closeNewInvoiceHander}
+                            refreshing={refreshHander}
+                        />
+                    )}
+                    <div className="app-content-header">
+                        <h1>Invoices</h1>
+                        <Button variant="filled" onClick={openNewInvoiceHander}>
+                            Add New Invoice
+                        </Button>
+                    </div>
+                    <div className="products-area-wrapper tableView">
+                        <div className="products-header">
+                            <div className="product-cell">ID</div>
+                            <div className="product-cell status-cell">
+                                Status
+                            </div>
+                            <div className="product-cell price">Cusotmer</div>
+                            <div className="product-cell price">Amount</div>
+                            <div className="product-cell price"></div>
+                        </div>
 
-                <InvoiceItemsTable invoice={invoiceData} />
-            </div>
+                        <InvoiceItemsTable invoice={invoiceData} />
+                    </div>
+                </>
+            ) : (
+                <Loader color="blue" type="dots" className="loader" size={50} />
+            )}
         </>
     );
 }
