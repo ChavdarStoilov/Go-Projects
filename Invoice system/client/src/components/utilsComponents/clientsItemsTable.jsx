782118b7d0a73cca78e875a8046ec80ecb7e6291
Chavdar Stoilov <80538498+ChavdarStoilov@users.noreply.github.com>
2024-05-13 09:42:08 +0300

    Added open details of Clients and ready for option update

--- Invoice system/client/src/components/utilsComponents/clientsItemsTable.jsx
+++ Invoice system/client/src/components/utilsComponents/clientsItemsTable.jsx
@@ -1,9 +1,45 @@
-import { Badge, Button, NumberFormatter } from "@mantine/core";
+import { Button, Modal, TextInput, Box } from "@mantine/core";
+import { useState } from "react";
 
 export default function ClientsItemsTable({ clients }) {
+    const [opened, setOpen] = useState(false);
 
     return (
         <>
+            {opened[0] && (
+                <Modal
+                    opened={opened[0]}
+                    onClose={() => setOpen(false)}
+                    title="Client details"
+                >
+                    <TextInput
+                        label="First name"
+                        required
+                        defaultValue={clients[opened[1]].first_name}
+                    />
+                    <TextInput
+                        label="Last name"
+                        required
+                        defaultValue={clients[opened[1]].last_name}
+                    />
+                    <TextInput
+                        label="Phone"
+                        required
+                        defaultValue={clients[opened[1]].phone}
+                    />
+                    <Box
+                        style={{
+                            marginTop: "20px",
+                            display: "flex",
+                            justifyContent: "space-evenly",
+                        }}
+                    >
+                        <Button>Save</Button>
+                        <Button onClick={() => setOpen(false)}>Close</Button>
+                    </Box>
+                </Modal>
+            )}
+
             {clients.length > 0 &&
                 clients.map((item, key) => (
                     <div className="products-row" key={key}>
@@ -25,6 +61,7 @@ export default function ClientsItemsTable({ clients }) {
                                 variant="gradient"
                                 size="compact-sm"
                                 gradient={{ from: "blue", to: "cyan", deg: 90 }}
+                                onClick={() => setOpen([true, key])}
                             >
                                 Open client
                             </Button>
