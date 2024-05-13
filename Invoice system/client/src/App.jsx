b7e74dc65c0d7d4c351482382048ac9bba6683c3
Chavdar Stoilov <80538498+ChavdarStoilov@users.noreply.github.com>
2024-05-13 09:16:22 +0300

    Added home loader

--- Invoice system/client/src/App.jsx
+++ Invoice system/client/src/App.jsx
@@ -1,5 +1,5 @@
 import HomePage from "./components/homePage";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Loader } from "@mantine/core";
 import { Routes, Route, useLocation } from "react-router-dom";
 import "@mantine/core/styles.css";
 import { useState, useEffect } from "react";
@@ -13,9 +13,9 @@ function App() {
     const location = useLocation();
     const [brandData, setBrandData] = useState(false);
     const [refresh, setRefresh] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const Data = () => setRefresh(true);
-
     useEffect(() => {
         api.GetBrandData()
             .then((result) => {
@@ -26,29 +26,64 @@ function App() {
             .catch((error) => {
                 console.log(error);
             })
-            .finally(() => setRefresh(false));
+            .finally(() => {
+                setRefresh(false);
+                setLoading(false);
+            });
     }, [refresh]);
 
     return (
         <MantineProvider>
-            <Menu brand={brandData.brandData ? brandData.brandData[0].name : false} />
-            <div className="app-content">
-                <Routes location={location} key={location.pathname}>
-                    <Route
-                        exact
-                        path="/"
-                        element={
-                            brandData.brandData ? (
-                                <HomePage brandData={brandData.brandData} counters={brandData.counters} />
-                            ) : (
-                                <NewBeandCreations addData={Data} />
-                            )
+            {!loading ? (
+                <>
+                    <Menu
+                        brand={
+                            brandData.brandData
+                                ? brandData.brandData[0].name
+                                : false
                         }
                     />
-                    <Route exact path="invoices/" element={<Invoices />} />
-                    <Route exact path="clients/" element={<Clients />} />
-                </Routes>
-            </div>
+                    <div className="app-content">
+                        <Routes location={location} key={location.pathname}>
+                            \
+                            <Route
+                                exact
+                                path="/"
+                                element={
+                                    brandData.brandData ? (
+                                        <HomePage
+                                            brandData={brandData.brandData}
+                                            counters={brandData.counters}
+                                        />
+                                    ) : (
+                                        <NewBeandCreations addData={Data} />
+                                    )
+                                }
+                            />
+                            <Route
+                                exact
+                                path="invoices/"
+                                element={<Invoices />}
+                            />
+                            <Route
+                                exact
+                                path="clients/"
+                                element={<Clients />}
+                            />
+                        </Routes>
+                    </div>
+                </>
+            ) : (
+                <div className="app-content">
+                    <Loader
+                        color="blue"
+                        type="dots"
+                        className="HomeLoading"
+                        size={50}
+                    />
+                    
+                </div>
+            )}
         </MantineProvider>
     );
 }
