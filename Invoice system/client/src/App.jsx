78af0cdc76cf3b96568ddaac4a3ab41b75c94383
Chavdar Stoilov <80538498+ChavdarStoilov@users.noreply.github.com>
2024-05-24 16:35:07 +0300

    Added error client handler and sent to server

--- Invoice system/client/src/App.jsx
+++ Invoice system/client/src/App.jsx
@@ -1,9 +1,12 @@
-import HomePage from "./components/homePage";
-import { MantineProvider, Loader } from "@mantine/core";
-import { Notifications } from '@mantine/notifications';
-import { Routes, Route, useLocation } from "react-router-dom";
+"use client";
 
-import '@mantine/notifications/styles.css';
+import HomePage from "./components/homePage";
+import { MantineProvider, Loader, rem } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { notifications } from "@mantine/notifications";
+import { IconExclamationMark } from "@tabler/icons-react";
+import "@mantine/notifications/styles.css";
 import "@mantine/core/styles.css";
 import { useState, useEffect } from "react";
 import * as api from "./api/data";
@@ -11,12 +14,16 @@ import Invoices from "./components/pages/invoices";
 import Clients from "./components/pages/clients";
 import NewBeandCreations from "./components/pages/newBrandCreation";
 import Menu from "./components/menu";
+import ErrorBoundaryPage from "./components/errorPage/error";
+
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
     const location = useLocation();
     const [brandData, setBrandData] = useState(false);
     const [refresh, setRefresh] = useState(false);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     const Data = () => setRefresh(true);
 
@@ -25,6 +32,20 @@ function App() {
             .then((result) => {
                 if (result.status === 200 && result.data !== null) {
                     setBrandData(result.data);
+                } else if (result.status === 500) {
+                    notifications.show({
+                        loading: false,
+                        autoClose: true,
+                        title: "Something went wrong..",
+                        message: `${result.data}, please contact your support.`,
+                        color: "red",
+                        icon: (
+                            <IconExclamationMark
+                                style={{ width: rem(18), height: rem(18) }}
+                            />
+                        ),
+                    });
+                    setError(true);
                 }
             })
             .catch((error) => {
@@ -35,6 +56,21 @@ function App() {
                 setLoading(false);
             });
     }, [refresh]);
+
+    const logError = (error, request) => {
+
+        const data = {
+            description : error
+        }
+
+        request(data)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    };
 
     return (
         <MantineProvider>
@@ -56,26 +92,70 @@ function App() {
                                 path="/"
                                 element={
                                     brandData.brandData ? (
-                                        <HomePage
-                                            brandData={brandData.brandData}
-                                            counters={brandData.counters}
-                                            refreshData={Data}    
-                                        />
-                                        
+                                        <ErrorBoundary
+                                            FallbackComponent={
+                                                ErrorBoundaryPage
+                                            }
+                                            onError={(error) =>
+                                                logError(
+                                                    `${error.message} on Home component!`, api.ErrorHandlerBrand
+                                                )
+                                            }
+                                        >
+                                            <HomePage
+                                                brandData={brandData.brandData}
+                                                counters={brandData.counters}
+                                                refreshData={Data}
+                                            />
+                                        </ErrorBoundary>
                                     ) : (
-                                        <NewBeandCreations addData={Data} />
+                                        !error && (
+                                            <NewBeandCreations addData={Data} />
+                                        )
                                     )
                                 }
                             />
                             <Route
                                 exact
                                 path="invoices/"
-                                element={brandData.brandData  && <Invoices brand={brandData.brandData}/>}
+                                element={
+                                    brandData.brandData && (
+                                        <ErrorBoundary
+                                            FallbackComponent={
+                                                ErrorBoundaryPage
+                                            }
+                                            onError={(error) =>
+                                                logError(
+                                                    `${error.message} on Invoice component!`, api.ErrorHandlerInvoice
+                                                )
+                                            }
+                                        >
+                                            <Invoices
+                                                brand={brandData.brandData}
+                                            />
+                                        </ErrorBoundary>
+                                    )
+                                }
                             />
                             <Route
                                 exact
                                 path="clients/"
-                                element={brandData.brandData  && <Clients />}
+                                element={
+                                    brandData.brandData && (
+                                        <ErrorBoundary
+                                            FallbackComponent={
+                                                ErrorBoundaryPage
+                                            }
+                                            onError={(error) =>
+                                                logError(
+                                                    `${error.message} on Clients component!`, api.ErrorHandlerInvoice
+                                                )
+                                            }
+                                        >
+                                            <Clients />
+                                        </ErrorBoundary>
+                                    )
+                                }
                             />
                         </Routes>
                     </div>
@@ -88,7 +168,6 @@ function App() {
                         className="HomeLoading"
                         size={50}
                     />
-                    
                 </div>
             )}
         </MantineProvider>
