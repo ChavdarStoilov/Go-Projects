592e0419998dbfc053f34363fb37ccc689739dca
Chavdar Stoilov <80538498+ChavdarStoilov@users.noreply.github.com>
2024-05-26 15:30:13 +0300

    Added desing for error page

--- Invoice system/client/src/components/errorPage/error.jsx
+++ Invoice system/client/src/components/errorPage/error.jsx
@@ -1,8 +1,20 @@
+import { Title, Text, Button, Container, Group } from "@mantine/core";
+import classes from "./error.module.css";
 
 export default function ErrorBoundaryPage() {
     return (
-        <>
-        <h1>error</h1>
-        </>
+        <Container style={{margin:"15% auto"}}>
+            <Title className={classes.title}>
+                Something bad just happened...
+            </Title>
+            <Text size="lg" ta="center" className={classes.description}>
+                Our client could not handle your request. <br />Please contact your vendor. <br />Try refreshing the page.
+            </Text>
+            <Group justify="center">
+                <Button variant="white" size="md" onClick={() => window.location.reload()}>
+                    Refresh the page
+                </Button>
+            </Group>
+        </Container>
     );
 }
